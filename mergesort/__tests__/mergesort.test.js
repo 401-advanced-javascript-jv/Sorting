f38a53306e7ddb4b5ab51a44128207f227c94a16
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-01 19:22:12 -0700

    Empty mergesort tests

---
+++ mergesort/__tests__/mergesort.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mergeSort = require('../mergesort.js');
+
+describe('mergeSort algorithm', () => {
+  it('should be able to deal with an empty array', ()=> {});
+  it('should be able to deal with a 1-element array', () => {});
+  it('should be able to sort a 2-element array', () => {});
+  it('should be able to sort a many-element array', () => {});
+});
